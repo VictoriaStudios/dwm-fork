33ce43973d1303b4355e7c293bd006d13a86ec5d
caesar <lel@lol.de>
2026-02-24 19:00:58 -0500

    brave now working

--- config.h
+++ config.h
@@ -29,6 +29,8 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "brave-browser",  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "brave",  NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
 /* layout(s) */
