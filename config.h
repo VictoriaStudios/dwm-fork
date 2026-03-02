f3614404a0746fc2ee6f36eeb5d95dd56cd86a7f
caesar <lel@lol.de>
2026-03-02 07:44:11 -0500

    gapps incorporated

--- config.h
+++ config.h
@@ -9,6 +9,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappx	    = 3;
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Medium:size=10:antialias=true:autohint=true" };
