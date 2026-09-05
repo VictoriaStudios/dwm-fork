72cbde4045b2a020ae5b2e5c0add8e18374d22d7
88744359+VictoriaStudios@users.noreply.github.com <88744359+VictoriaStudios@users.noreply.github.com>
2026-09-05 13:15:50 -0500

    Added icedove screen allocations

--- config.h
+++ config.h
@@ -34,7 +34,6 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-
 static const Rule rules[] = {
     /* class            instance    title       tags mask     isfloating   monitor */
     { "Gimp",           NULL,       NULL,       0,            1,           -1 },
@@ -50,7 +49,9 @@ static const Rule rules[] = {
     { "steam",      NULL,       NULL,       1 << 4,           0,           0 },
     { "pa-applet",      NULL,       NULL,       ~0,           1,           -1 },
     { "Mail",      NULL,       NULL,       1 << 7,           0,           -1 },
-    { "Thunderbird",      NULL,       NULL,       1 << 7,           0,           -1 }
+    { "Thunderbird",      NULL,       NULL,       1 << 7,           0,           -1 },
+    { "Mail",      NULL,       NULL,       1 << 7,           0,           -1 },
+    { "Icedove",      NULL,       NULL,       1 << 7,           0,           -1 }
 };
 
 /* layout(s) */
